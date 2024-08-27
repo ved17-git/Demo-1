11e0e801908d723bed7a813d4406ef07ab61fcb9
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2024-08-27 19:30:45 +0530

    deleted server

--- server/utils/token.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const generateToken=(user)=>{
-    return jwt.sign({
-        email:user.email,
-        _id:user._id
-      },process.env.PrivateKey,{ expiresIn: '1h' })
-}
-
